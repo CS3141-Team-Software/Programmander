27577a69dda469b8e48338b923eb495992db67b1
Caleb Chapman <cgchapma@c28-0117-03.ad.mtu.edu>
2014-10-02 17:43:59 -0400

    Pushing Graph and Graphnode implementations

--- gameEngine/Graph.java
+++ gameEngine/Graph.java
@@ -1,5 +1,69 @@
 package gameEngine;
 
+//import GraphNode;
+
+
 public class Graph {
 
-}
+	private int size;
+	private GraphNode[][] map;
+	private int rows;
+	private int cols;
+
+
+	/*
+	 * Constructor
+	 */
+	public Graph(int x, int y){
+
+		size = x*y;
+		rows = y;
+		cols = x;
+		map = new GraphNode[y][x];
+
+
+		//making an empty map...
+		for(int i = 0; i < y; i++){
+			for(int j = 0; j < x; j++){
+				map[i][j] = new GraphNode(i, j);
+			}
+		}
+
+		//Connect all nodes together
+		for(int i = 0; i < y ; i++){
+			for(int j = 0; j < x ; j++){
+				//North
+				if(i > 0){
+					map[i][j].setNNode(map[i-1][j]);
+				}
+				//South
+				if(i < y-1){
+					map[i][j].setSNode(map[i+1][j]);
+				}
+				//East
+				if(j < x-1){
+					map[i][j].setENode(map[i][j+1]);
+				}
+				//West
+				if(j > 0){
+					map[i][j].setWNode(map[i][j-1]);
+				}
+			}
+		}
+	}
+
+	/*
+	 * Get Node
+	 * returns the node at the specified coordinates 
+	 */
+	public GraphNode getNode(int x, int y){
+		if(x >= cols || y >= rows){
+			return null;
+		}
+		else{
+			return map[y][x];
+		}
+	}
+}	
+
+
--- gameEngine/GraphNode.java
+++ gameEngine/GraphNode.java
@@ -1,5 +1,153 @@
 package gameEngine;
 
-public class GraphNode {
+//import GraphNode;
+import Obstruction;
 
+/*
+ * Programmander Graph Node
+ * Caleb Chapman
+ */
+public class GraphNode {
+	
+	private GraphNode north;
+	private GraphNode south;
+	private GraphNode east;
+	private GraphNode west;
+	private int row;
+	private int col;
+	private Obstruction obs;
+	//private actor[] actors;
+	
+	/*
+	 * Constructor for making an individual node
+	 * ...for whatever
+	 */
+	public GraphNode(GraphNode n, GraphNode s, GraphNode e, GraphNode w){
+		north = n;
+		south = s;
+		east = e;
+		west = w;
+	}
+	
+	/*
+	 * Constructor the graph uses for
+	 * making nodes
+	 */
+	public GraphNode(int y,int x){
+		north = null;
+		south = null;
+		east = null;
+		west = null;
+		row = y;
+		col = x;
+	}
+	
+	/*
+	 * Getter Methods for
+	 * North South East West 
+	 */
+	public GraphNode getNNode(){
+		return north;
+	}
+	public GraphNode getSNode(){
+		return south;
+	}
+	public GraphNode getENode(){
+		return east;
+	}
+	public GraphNode getWNode(){
+		return west;
+	}
+	
+	public GraphNode getNENode(){
+		if(north == null){
+			return null;
+		}
+		if(north.getENode() != null){
+			return north.getENode();
+		}
+		else{
+			return null;
+		}
+	}
+	
+	/*
+	 * Getter Methods for
+	 * NorthEast SouthEast
+	 * NorthWest SouthWest
+	 */
+	public GraphNode getSENode(){
+		if(south == null){
+			return null;
+		}
+		if(south.getENode() != null){
+			return south.getENode();
+		}
+		else{
+			return null;
+		}
+	}
+	
+	public GraphNode getNWNode(){
+		if(north == null){
+			return null;
+		}
+		if(north.getWNode() != null){
+			return north.getWNode();
+		}
+		else{
+			return null;
+		}
+	}
+	
+	public GraphNode getSWNode(){
+		if(south == null){
+			return null;
+		}
+		if(south.getWNode() != null){
+			return south.getWNode();
+		}
+		else{
+			return null;
+		}
+	}
+	
+	public Obstruction getObstruction(){
+		return obs;
+	}
+	/*
+	 * End of Getter Methods
+	 */
+	
+	/*
+	 * Setter Methods
+	 */
+	public void setNNode(GraphNode n){
+		north = n;
+	}
+	public void setSNode(GraphNode s){
+		south = s;
+	}
+	public void setENode(GraphNode e){
+		east = e;
+	}
+	public void setWNode(GraphNode w){
+		west = w;
+	}
+	
+	public void setObstruction(Obstruction o){
+		obs = o;
+	}
+	/*
+	 * End of Setter Methods
+	 */
+	
+	/*
+	 * getCoordinates
+	 * returns the coordinates of the node as 
+	 * they were set in the constructor
+	 */
+	public String getCoordinates(){
+		return ("[Row: " + row + ", Col: " + col + "]");
+	}
 }
