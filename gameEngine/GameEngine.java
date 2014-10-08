763ebeb7c154dc82c6971f193c7089259f75eaac
fwnolte <fwnolte@mtu.edu>
2014-10-08 15:31:24 -0400

    Mini-commit to get updated code from everyone else

--- gameEngine/GameEngine.java
+++ gameEngine/GameEngine.java
@@ -2,6 +2,8 @@ package gameEngine;
 
 import java.util.ArrayList;
 
+import javax.swing.JPanel;
+
 
 /**
  * @author Frederick Nolte
@@ -9,30 +11,50 @@ import java.util.ArrayList;
  */
 public class GameEngine {
 
+	private boolean gameOver = false;
 	private Graph map;
-	private GraphNode[][] nodes;
-
+	private GraphNode[] nodes;
+	private JPanel window;
+	private ArrayList<Actor> actors;
+	
 	public GameEngine(String mapName, String firstAIName, String difficulty, boolean is2Player) {
 
-		
 		//Make our map using data passed in by LauncherWindow
 		map = new Graph(mapName);
-		nodes = map.getNodes();
-		
+		nodes = map.getNodesSing();
+		actors = map.getActors();
 
 		//Initialize 1st player's AI by calling Mark's magic code.
 		//TODO: Implement Mark's magic code
-		
+
 		if (is2Player) {
 			//Initialize 2nd player's AI
 		} else {
 			//Initialize our AI based on which difficulty was selected
 		}
-
-
+		
+		this.run();
 
 	}
 
+	//Run the game!
+	private void run() {
+		while(!gameOver) {
+			
+			//Go through actors and update them
+			for (Actor a : actors) {
+				a.update();
+			}
+			
+			//Paint to buffered image
+			
+		}
+		
+	}
 
+	//Sets reference to our drawing area so we can draw in it.
+	public void setGamePanel(JPanel w) {
+		window = w;
+	}
 }
 
--- gameWindow/GameWindow.java
+++ gameWindow/GameWindow.java
@@ -12,6 +12,7 @@ import javax.swing.JLabel;
 import javax.swing.JButton;
 
 public class GameWindow extends JFrame {
+	
 	public GameWindow(GameEngine gameEngine){
 		setSize(new Dimension(1720, 880));
 		getContentPane().setPreferredSize(new Dimension(1720, 880));
@@ -27,14 +28,14 @@ public class GameWindow extends JFrame {
 		btnDummyButton.setBounds(1529, 24, 175, 25);
 		getContentPane().add(btnDummyButton);
 		
-		JPanel panel = new JPanel();
-		panel.setPreferredSize(new Dimension(800, 800));
-		panel.setMinimumSize(new Dimension(800, 800));
-		panel.setMaximumSize(new Dimension(800, 800));
-		panel.setSize(new Dimension(800, 800));
-		panel.setBounds(12, 24, 800, 800);
-		getContentPane().add(panel);
-		panel.setBackground(Color.RED);
+		JPanel game = new JPanel();
+		game.setPreferredSize(new Dimension(800, 800));
+		game.setMinimumSize(new Dimension(800, 800));
+		game.setMaximumSize(new Dimension(800, 800));
+		game.setSize(new Dimension(800, 800));
+		game.setBounds(12, 24, 800, 800);
+		getContentPane().add(game);
+		game.setBackground(Color.RED);
 		
 	}
 	public void Stuff() {
