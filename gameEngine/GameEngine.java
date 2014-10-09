d90af35699f4c700cc9b0c33ad5aab6e9d243d1f
fwnolte <fwnolte@mtu.edu>
2014-10-09 18:59:30 -0400

    Added functionality to work towards rendering the game

--- gameEngine/GameEngine.java
+++ gameEngine/GameEngine.java
@@ -19,47 +19,70 @@ public class GameEngine {
 	private GraphNode[] nodes;
 	private JPanel window;
 	private ArrayList<Actor> actors;
-	//Used to store image data
+	private int xDim, yDim;
+	
+	//Used to store image data for caching
 	private Dictionary<String, Image> cache;
-	//Used to paint our image to the frame.
+	//Used to paint our image to the GameWindow.
 	private BufferedImage currFrame;
+	private BufferedImage nextFrame;
 	
 	public GameEngine(String mapName, String firstAIName, String difficulty, boolean is2Player) {
 
 		//Make our map using data passed in by LauncherWindow
 		map = new Graph(mapName);
 		nodes = map.getNodesSing();
-		//TODO: Need this to actually happen
 		actors = map.getActors();
-
+		
+		currFrame = null;
+		nextFrame = new BufferedImage(xDim, yDim, BufferedImage.TYPE_INT_RGB);
 		//Initialize 1st player's AI by calling Mark's magic code.
 		//TODO: Implement Mark's magic code
 
 		if (is2Player) {
-			//Initialize 2nd playertring, image's AI
+			//Initialize 2nd player's AI
 		} else {
 			//Initialize our AI based on which difficulty was selected
 		}
 		
-		this.run();
+		generateImage(nextFrame);
+	}
 
+	//Paint image into the buffered image
+	private void generateImage(BufferedImage image) {
+		// TODO Auto-generated method stub
+		
 	}
 
 	//Run the game!
-	private void run() {
+	public void run() {
 		while(!gameOver) {
+		
 			
-			//Go through actors and update them
-			for (Actor a : actors) {
-				a.update();
+			//Paint to our nextFrame
+			
+			for (GraphNode g : nodes) {
+				//Get background data
+				//Check cache for that image
+				//Paint that image to nextFrame based on coordinates of that frame
+				//Get actor data
+				//Check cache for that image
+				//Paint that image to nextFrame based on coordinates of that frame
 			}
 			
-			//Paint to buffered image
+			//Paint nextFrame to the screen
+			
+			
+			currFrame = nextFrame; 
+			nextFrame = new BufferedImage(xDim, 800, BufferedImage.TYPE_INT_RGB);
+			
+			
 			
 		}
 		
 	}
 
+	//TODO: IS this needed?
 	//Sets reference to our drawing area so we can draw in it.
 	public void setGamePanel(JPanel w) {
 		window = w;
