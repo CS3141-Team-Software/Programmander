015af2731e7f0d6bcd824ffb1560544025371939
fwnolte <fwnolte@mtu.edu>
2014-11-11 18:33:40 -0500

    Wrote save class functionality for editor window

--- editor/src/editorWindow/EditorWindow.java
+++ editor/src/editorWindow/EditorWindow.java
@@ -3,6 +3,7 @@ package editorWindow;
 import java.awt.Component;
 import java.awt.Dimension;
 import java.io.BufferedWriter;
+import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 
@@ -13,7 +14,11 @@ import org.fife.ui.rsyntaxtextarea.SyntaxConstants;
 import org.fife.ui.rtextarea.RTextScrollPane;
 
 public class EditorWindow extends JFrame {
-
+	
+	private File currFile;
+	private String currUnitType;
+	private String currFileName;
+	
 	public EditorWindow() {
 
 		Dimension d = java.awt.Toolkit.getDefaultToolkit().getScreenSize();
@@ -21,7 +26,7 @@ public class EditorWindow extends JFrame {
 		this.setMinimumSize(d);
 		this.setMaximumSize(d);
 		this.setPreferredSize(d);
-		
+
 		//These variables are our available screensize, including borders
 		int innerWidth = (int)d.getWidth() - getInsets().left - getInsets().right;
 		int innerHeight = (int)d.getHeight() - getInsets().top - getInsets().bottom - 130; //-130 for reasons
@@ -35,39 +40,47 @@ public class EditorWindow extends JFrame {
 		textArea.setCodeFoldingEnabled(true);
 
 		RTextScrollPane sp = new RTextScrollPane(textArea);
-		
+
 		int textWidth = (int)(innerWidth *.66);
 		sp.setBounds(10, 50, textWidth, innerHeight);
 		sp.setVisible(true);
 		textArea.setVisible(true);
 		getContentPane().add(sp);
-		
+
 		setTitle("Text Editor Demo");
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
-	
+
 	}
-	
-	private String openFile(String fileName) {
-		
+
+	private String openFile(String fileName, String classType) {
+		currFileName = fileName + ".java";
+		currFile = new File(fileName);
 		return null;
 	}
-	
-	private String saveClass (String fileName, String className) {
+
+	private void saveClass (String textAreaText) {
 		try {
-			BufferedWriter writer = new BufferedWriter(new FileWriter(fileName + ".txt"));
+			//Will append to end of our half-created file.
+			BufferedWriter writer = new BufferedWriter(new FileWriter(currFile, true));
+			
+			writer.write("\n");
+			//Write the FULL text of the update() method to the file
+			writer.write(textAreaText);			
+			
+			writer.write("\n}");
+			
 		} catch (Exception e) {
 			e.printStackTrace();
 			System.out.println("Error writing to a class file");
 			System.exit(-1);
 		}
-		return null;
 	}
-	
+
 	private String saveSpawner(String fileName, String spawnerName) {
-		
+
 		return null;
 	}
-	
-	
+
+
 
 }
